9dfa1a48f061aead84f580997e817154983661f3
zcy <1832244088@qq.com>
2022-02-14 10:57:31 +0800

    图片懒加载

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -25,6 +25,10 @@ export const useLayzData = (apiFn) => {
           result.value = data.result
         })
       }
+    },
+    // 配置选项:相交的比例大于0就触发(懒加载的盒子与可视区域相交的比例)
+    {
+      threshold: 0
     }
   )
   return { result, target }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -13,6 +13,12 @@
     <HomeNew />
     <!-- 人气推荐 -->
     <HomeHot />
+    <!-- 品牌组件 -->
+    <HomeBrand />
+    <!-- 商品区块 -->
+    <HomeProduct />
+    <!-- 最新专题 -->
+    <HomeSpecial />
   </div>
 </template>
 
@@ -21,13 +27,19 @@ import HomeCategory from './components/home-category.vue'
 import HomeBanner from './components/home-banner.vue'
 import HomeNew from './components/home-new.vue'
 import HomeHot from './components/home-hot.vue'
+import HomeBrand from './components/home-brand.vue'
+import HomeProduct from './components/home-product.vue'
+import HomeSpecial from './components/home-special.vue'
 export default {
   name: 'HomePage',
   components: {
     HomeCategory,
     HomeBanner,
     HomeNew,
-    HomeHot
+    HomeHot,
+    HomeBrand,
+    HomeProduct,
+    HomeSpecial
   }
 }
 </script>
