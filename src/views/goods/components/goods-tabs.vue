5e0bc5028166db1606114e586d0cd6902860e32d
zcy <1832244088@qq.com>
2022-03-03 11:13:29 +0800

    商品详情和评价组件

---
+++ src/views/goods/components/goods-tabs.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="goods-tabs">
+    <nav>
+      <a
+        @click="activeName = 'GoodsDetail'"
+        :class="{ active: activeName === 'GoodsDetail' }"
+        href="javascript:;"
+        >商品详情</a
+      >
+      <a
+        @click="activeName = 'GoodsComment'"
+        :class="{ active: activeName === 'GoodsComment' }"
+        href="javascript:;"
+        >商品评价<span>({{ goods.commentCount }})</span></a
+      >
+    </nav>
+    <!-- 切换内容的地方 : 使用动态组件 -->
+    <!-- is属性的值写组件的名称即可 -->
+    <keep-alive>
+      <component :is="activeName"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import { ref, inject } from 'vue'
+import GoodsDetail from './goods-detail.vue'
+import GoodsComment from './goods-comment.vue'
+export default {
+  name: 'GoodsTabs',
+  components: {
+    GoodsDetail,
+    GoodsComment
+  },
+  setup () {
+    // 默认为商品详情
+    const activeName = ref('GoodsDetail')
+    // goods详情数据
+    const goods = inject('goods')
+    return { activeName, goods }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.goods-tabs {
+  min-height: 600px;
+  background: #fff;
+  nav {
+    height: 70px;
+    line-height: 70px;
+    display: flex;
+    border-bottom: 1px solid #f5f5f5;
+    a {
+      padding: 0 40px;
+      font-size: 18px;
+      position: relative;
+      > span {
+        color: @priceColor;
+        font-size: 16px;
+        margin-left: 10px;
+      }
+      &:first-child {
+        border-right: 1px solid #f5f5f5;
+      }
+      &.active {
+        &::before {
+          content: "";
+          position: absolute;
+          left: 40px;
+          bottom: -1px;
+          width: 72px;
+          height: 2px;
+          background: @xtxColor;
+        }
+      }
+    }
+  }
+}
+</style>
